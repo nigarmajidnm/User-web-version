81635213fc15cff7af9f6ad8f05f3bb2ebe8d7ee
nigarmajidnm <majidovanigar9@gmail.com>
2024-10-22 01:46:12 +0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
   */
 export const environment = {
   production: false,
-  baseURL: 'https://api.domain.com/public/api/',
-  mediaURL: 'https://api.domain.com/public/storage/images/',
+  baseURL: 'https://multi-salon.onrender.com/public/api/',
+  mediaURL: 'https://multi-salon.onrender.com/public/storage/images/',
   app_name: 'Beauty Link',
   websiteURL: 'https://initappz.com',
   websiteName: 'initappz',
